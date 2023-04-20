4940506787456d14039bc0c06fa8a5458f85e560
Rezaavoor <Reza.avoor@gmail.com>
2023-04-20 16:32:34 +0200

    implement tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      primary: "#FFB563",
+      primaryDark: "#F85E00",
+      primaryLight: "#FFD29D",
+      secondaryLight: "#918450",
+      secondaryDark: "#A41623", 
+    }
+  },
+  plugins: [],
+}
