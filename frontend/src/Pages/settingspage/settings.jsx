68570dacadfd654421a42ab0607e8095cea2cb09
olofolssonsrc <olofolsson112233@gmail.com>
2023-04-24 14:38:52 +0200

    add header to setting page

--- frontend/src/Pages/settingspage/settings.jsx
+++ frontend/src/Pages/settingspage/settings.jsx
@@ -4,11 +4,20 @@ import ProfileIcon from '../../Components/icons/ProfileIcon'
 import UserTermsIcon from '../../Components/icons/UserTermsIcon'
 import LogoutIcon from '../../Components/icons/LogoutIcon'
 import AchievementsIcon from '../../Components/icons/AchievementsIcon'
+import LogoLarge from "../../Components/icons/LogoLarge"
 export default function settings() {
   return (
     <div className="bg-white h-screen m-auto w-screen max-w-md">
-        <div>header</div>
-        <div className=''>
+        <div className="h-1/3 ">
+
+          <div className="flex flex-col justify-center h-full">  
+            <div className="flex flex-row justify-center">
+                    <LogoLarge/>
+            </div>
+          </div>
+        
+        </div>
+        <div className='items-center flex flex-col h-1/2 justify-between'>
           <Button text="About me" icon={ProfileIcon} size="large"></Button>
           <Button text="Achievements" icon={AchievementsIcon} size="large"></Button>
           <Button text="User Terms" icon={UserTermsIcon} size="large"></Button>
