542dd2ef355958c31f7621e08a403f8ea4899632
Phoenix Connelin <126803905+PhoenixConnelin@users.noreply.github.com>
2023-04-24 13:00:24 +0200

    Added code to TagCard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import CultureGo from "./Components/icons/CultureGo";
 import HomeIcon from "./Components/icons/HomeIcons";
 import Tags from "./Pages/Tagpage/tags";
+import TagCard from "./Pages/Tagpage/TagCard";
 
 function App() {
   return (
     <div>
-      <Tags />
+      <TagCard />
     </div>
   );
 }
--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import Outdoor from '../../Components/icons/tag-page-icons/Outdoor.svg'
 
 export default function TagCard() {
   return (
-    <div className='bg-[url("../../Components/icons/tag-page-icons/Outdoor.svg")]'>
-
+    <div class = "bg-[url('../../Components/icons/tag-page-icons/Outdoor.svg')]">
     </div>
   )
 }
