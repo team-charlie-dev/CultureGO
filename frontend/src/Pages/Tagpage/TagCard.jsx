e6d1d9cda9b3674408c506eb548dd7db78e09eab
Pontus <ponta.berglund.pb@gmail.com>
2023-04-24 11:31:14 +0200

    Add svg images

---
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TagCard() {
+  return (
+    <div className='bg-[url("../../Components/icons/tag-page-icons/Outdoor.svg")]'>
+
+    </div>
+  )
+}
--- frontend/src/Pages/Tagpage/tags.jsx
+++ frontend/src/Pages/Tagpage/tags.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import Outdoor from '../../Components/icons/tag-page-icons/Outdoor'
+import Outdoor from '../../Components/icons/tag-page-icons/Outdoor.svg'
+import Indoor from '../../Components/icons/tag-page-icons/Indoor.svg'
+import Free from '../../Components/icons/tag-page-icons/Free.svg'
+import Random from '../../Components/icons/tag-page-icons/Random.svg'
 export default function Tags() {
   return (
     <div className='flex flex-row'>
-        <div className='flex flex-col'>
-            <div>Tag 1
-              <Outdoor/>
-            </div>
-            <div>Tag 2</div>
+      <div className='flex flex-col'>
+        <div>
+          <img src={Outdoor} />
         </div>
-        <div className='flex flex-col   '>
-            <div>Tag 3</div>
-            <div>Tag 4</div>
+        <div>
+          <img src={Indoor} />
         </div>
+      </div>
+      <div className='flex flex-col   '>
+        <div>
+          <img src={Free} />
+        </div>
+        <div>
+          <img src={Random} />
+        </div>
+      </div>
     </div>
   )
 }
