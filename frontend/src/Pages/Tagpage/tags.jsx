d38a901d225fae6167aa8fd13f68c5f163b996c8
Phoenix Connelin <126803905+PhoenixConnelin@users.noreply.github.com>
2023-04-24 11:10:37 +0200

    Added for tag page

--- frontend/src/Pages/Tagpage/tags.jsx
+++ frontend/src/Pages/Tagpage/tags.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Outdoor from '../../Components/icons/tag-page-icons/Outdoor'
 export default function Tags() {
   return (
     <div className='flex flex-row'>
         <div className='flex flex-col'>
-            <div>Tag 1</div>
+            <div>Tag 1
+              <Outdoor/>
+            </div>
             <div>Tag 2</div>
         </div>
         <div className='flex flex-col   '>
