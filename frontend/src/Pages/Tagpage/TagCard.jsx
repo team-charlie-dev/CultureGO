e81ed10a2e7e412492fe8189e25d162a02478b28
Pontus <ponta.berglund.pb@gmail.com>
2023-04-24 15:40:58 +0200

    Change to grid.

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function TagCard({image, text}) {
   return (
-    <div style = {{backgroundImage: `url(${image})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat'}}
-    className='flex items-center justify-center w-16 h-24'>
+    <div style = {{backgroundImage: `url(${image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}
+    className='flex items-center justify-center bg-primary border-primary'>
       <p className='text-center bg-black bg-opacity-40 text-white cneter w-full'>{text}</p>
     </div>
   )
--- frontend/src/Pages/Tagpage/tags.jsx
+++ frontend/src/Pages/Tagpage/tags.jsx
@@ -7,23 +7,12 @@ import TagCard from './TagCard'
 
 export default function Tags() {
   return (
-    <div className='flex flex-row'>
-      <div className='flex flex-col'>
-        <div>
+    <div className='grid grid-cols-2 h-screen'>
+          <p className='absolute bg-white'>What mood are you in?</p>
           <TagCard image={Outdoor} text = "Outdoor"/>
-        </div>
-        <div>
           <TagCard image={Indoor} text = "Indoor"/>
-        </div>
-      </div>
-      <div className='flex flex-col   '>
-        <div>
           <TagCard image={Free} text = "Free"/>
-        </div>
-        <div>
           <TagCard image={Random} text = "Random"/>
-        </div>
-      </div>
     </div>
   )
 }
