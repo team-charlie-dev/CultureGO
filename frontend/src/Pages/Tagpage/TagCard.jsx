7f9666dcb02a5435db05ef2e058328395a6394a4
Phoenix Connelin <126803905+PhoenixConnelin@users.noreply.github.com>
2023-04-24 16:01:16 +0200

    Adjusted cards position

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function TagCard({image, text}) {
+export default function TagCard({image, text, position}) {
   return (
-    <div style = {{backgroundImage: `url(${image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}
+    <div style = {{backgroundImage: `url(${image})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: `${position}`}}
     className='flex items-center justify-center bg-primary border-primary'>
       <p className='text-center bg-black bg-opacity-40 text-white cneter w-full'>{text}</p>
     </div>
--- frontend/src/Pages/Tagpage/tags.jsx
+++ frontend/src/Pages/Tagpage/tags.jsx
@@ -7,12 +7,14 @@ import TagCard from './TagCard'
 
 export default function Tags() {
   return (
-    <div className='grid grid-cols-2 h-screen'>
-          <p className='absolute bg-white'>What mood are you in?</p>
-          <TagCard image={Outdoor} text = "Outdoor"/>
-          <TagCard image={Indoor} text = "Indoor"/>
-          <TagCard image={Free} text = "Free"/>
-          <TagCard image={Random} text = "Random"/>
+    <div>
+      <p className='bg-white text-center relative top-28 '>What mood are you in?</p>
+      <div className='grid grid-cols-2 h-screen'>
+          <TagCard image={Outdoor} text = "Outdoor" position = 'right'/>
+          <TagCard image={Indoor} text = "Indoor" position = 'left'/>
+          <TagCard image={Free} text = "Free" position = 'right'/>
+          <TagCard image={Random} text = "Random" position = 'left'/>
+      </div>
     </div>
   )
 }
