1786d7018b870392aca81612495171afa118d382
Phoenix Connelin <126803905+PhoenixConnelin@users.noreply.github.com>
2023-04-24 13:50:46 +0200

    Updated textview in TagCard

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function TagCard({image, text}) {
   return (
     <div style = {{backgroundImage: `url(${image})`, backgroundSize: 'cover'}}
-    className='w-16 h-24'>
-      <p className='text-center bg-black bg-opacity-40 text-white'>{text}</p>
+    className='w-16 h-24 flex items-center justify-center'>
+      <p className='text-center bg-black bg-opacity-40 text-white w-full'>{text}</p>
     </div>
   )
 }
