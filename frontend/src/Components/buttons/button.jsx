c75c049350faa6b34cf355b5c745a3e5932cd61b
Rezaavoor <Reza.avoor@gmail.com>
2023-04-24 14:00:30 +0200

    add button component

--- frontend/src/Components/buttons/button.jsx
+++ frontend/src/Components/buttons/button.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 export default function button({text, icon, size}) {
-    return (
-        <div>
-            {text}
-        </div>
-    )
+    if(size === 'large'){
+        return (
+            <div className='bg-primaryDark rounded-2xl h-16 w-5/6 m-6 text-center flex justify-around px-16 text-white font-bold italic relative text-lg'>
+                <div className='h-full'>
+                    {icon()}
+                </div>
+                <div className='m-auto'>
+                    {text}
+                </div>
+            </div>
+        )
+    }
+    else if(size === 'small'){
+        return(
+            <div className='bg-primaryDark h-6 w-44 m-6 text-center flex justify-around px-16 text-white font-bold relative text-base'>
+                <div className='h-full'>
+                    {icon()}
+                </div>
+                <div className='m-auto'>
+                    {text}
+                </div>
+            </div>
+        )
+    }
 }
