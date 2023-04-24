e1fd66941c634fd23f06efb56c07ef95dda1b724
Pontus <ponta.berglund.pb@gmail.com>
2023-04-24 14:11:08 +0200

    Update TagCard.jsx

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function TagCard({image, text}) {
   return (
-    <div style = {{backgroundImage: `url(${image})`, backgroundSize: 'cover'}}
-    className='w-16 h-24 flex items-center justify-center'>
-      <p className='text-center bg-black bg-opacity-40 text-white w-full'>{text}</p>
+    <div style = {{backgroundImage: `url(${image})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat'}}
+    className='flex items-center justify-center w-16 h-24'>
+      <p className='text-center bg-black bg-opacity-40 text-white cneter w-full'>{text}</p>
     </div>
   )
 }
