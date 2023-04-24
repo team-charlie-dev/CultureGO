6d806fa6b90b4f779bd652be644b93635416ab63
Rezaavoor <Reza.avoor@gmail.com>
2023-04-24 14:01:02 +0200

    make buttons work on Settings page

--- frontend/src/Pages/settingspage/settings.jsx
+++ frontend/src/Pages/settingspage/settings.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import Button from '../../Components/buttons/button'
+import ProfileIcon from '../../Components/icons/ProfileIcon'
+import UserTermsIcon from '../../Components/icons/UserTermsIcon'
+import LogoutIcon from '../../Components/icons/LogoutIcon'
+import AchievementsIcon from '../../Components/icons/AchievementsIcon'
 export default function settings() {
-    return (
-        <div className="bg-white h-screen m-auto w-screen max-w-sm text-blue">
-            <h1 className="bg-blue">
-                hej
-            </h1>
-                <div>header</div>
-
-                <div>
-
-                    <Button text="About me" icon="" size="large"></Button>
-                    <Button text="About me" icon="" size="large"></Button>
-                    <Button text="About me" icon="" size="large"></Button>
-                    <Button text="About me" icon="" size="large"></Button>
-
-                </div>
+  return (
+    <div className="bg-white h-screen m-auto w-screen max-w-md">
+        <div>header</div>
+        <div className=''>
+          <Button text="About me" icon={ProfileIcon} size="large"></Button>
+          <Button text="Achievements" icon={AchievementsIcon} size="large"></Button>
+          <Button text="User Terms" icon={UserTermsIcon} size="large"></Button>
+          <Button text="Logout" icon={LogoutIcon} size="large"></Button>
 
         </div>
-    )
+
+    </div>
+  )
 }
