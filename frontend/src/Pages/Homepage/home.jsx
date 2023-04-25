dd44ef50215a1b85594246a45058e125f44e2400
Dglaas <david.glaas@gmail.com>
2023-04-25 15:33:42 +0200

    Update homepage

--- frontend/src/Pages/Homepage/home.jsx
+++ frontend/src/Pages/Homepage/home.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import Like from "../../Components/icons/Like.svg";
 import Dislike from "../../Components/icons/Dislike.svg";
 import Logo from "../../Components/icons/Logo.svg";
-import SelectCity from "../../Components/buttons/selectCity"
-import City from "../../Components/icons/City"
+import SelectCity from "../../Components/buttons/selectCity";
+import City from "../../Components/icons/City";
+import Arrow from "../../Components/icons/Arrow";
 
 // const images = [HomeIcon, CultureGo]
 
 const home = () => {
   return (
     <div className="bg-white">
-       <Header />
+      <Header />
       <div className="flex justify-end">
-      <CitySelector />
+        <CitySelector />
       </div>
 
       <div className="bg-white flex flex-col gap-10 h-screen">
-      <div className="relative h-[65%] w-auto font-inriaSans">
-        <div className="bg-gradient-to-t from-transparent from-30% to-white absolute top-0 bottom-0 left-0 right-0 z-10">
+        <div className="relative h-[65%] w-auto font-inriaSans p-3">
+          <div className="bg-gradient-to-t from-transparent from-30% to-white absolute top-0 bottom-0 left-0 right-0 z-10"></div>
+          <Image />
+          <div className="absolute bottom-0 left-0 right-0 p-3">
+            <InfoBox />
+          </div>
         </div>
-        <Image />
-        <div className="absolute bottom-0 left-0 right-0">
-          <InfoBox />
-        </div>
+        <Buttons />
       </div>
-      <Buttons />
     </div>
-    </div>
-
   );
 };
 
@@ -38,7 +37,7 @@ const Image = () => {
     "https://www.city-guide-stockholm.com/_bibli/annonces/455/hd/abba-museum-03.jpg";
   return (
     <div className="h-full">
-      <img src={img} className="rounded-[30px] object-none h-full"></img>
+      <img src={img} className="rounded-b-[30px] object-none h-full"></img>
     </div>
   );
 };
@@ -53,6 +52,9 @@ const InfoBox = () => {
         doloremque consequuntur, voluptatum nesciunt reprehenderit eius enim
         labore, modi assumenda perspiciatis obcaecati?
       </p>
+      <div className="flex justify-end">
+        <Arrow />
+      </div>
     </div>
   );
 };
@@ -68,19 +70,37 @@ const Buttons = () => {
 
 const Header = () => {
   return (
-    <header className="p-3">
+    <header className="p-3 pt-10">
       <img src={Logo}></img>
     </header>
   );
 };
 
 const CitySelector = () => {
-
   function clickHandle() {
-    console.log("clicked")
+    console.log("clicked");
   }
 
-  return(
-    <SelectCity text="Stockholm" icon={City} size="small" clickHandler={clickHandle} />
-  )
-}
+  return (
+    <SelectCity
+      text="Stockholm"
+      icon={City}
+      size="small"
+      clickHandler={clickHandle}
+    />
+  );
+};
+
+const getPicture = async () => {
+  return await fetch(
+    "https://iynsfqmubcvdoqicgqlv.supabase.co/storage/v1/object/public/team-charlie-storage/charlie.jpg",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+const ArrowButton = () => {};
