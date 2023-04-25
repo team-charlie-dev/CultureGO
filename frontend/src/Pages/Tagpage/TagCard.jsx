7dd9960224e026993c83e51f59e45d55247c4426
Pontus <ponta.berglund.pb@gmail.com>
2023-04-25 19:22:07 +0200

    Add black box behind text

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -16,8 +16,8 @@ export default function TagCard({ image, text, xPosition, yPosition }) {
       <div style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: `${xPosition}`, backgroundPositionY: `${yPosition}` }}
         className={`transition-all duration-500 w-full h-full ${clicked ? clickedStyle : ''}`}>
       </div>
-      <div className={`transition-all duration-500 ${clicked ? '' : ''} bg-opacity-50 text-center absolute`}>
-        <p className={`text-white font-inriaSans italic`}>{text}</p>
+      <div className={`transition-all duration-500 ${clicked ? '' : ''} absolute`}>
+        <p className={`text-white font-inriaSans italic ${clicked ? '' : 'bg-black bg-opacity-50 rounded-full'} transition-all duration-500 px-3`}>{text}</p>
         <img src = {Logo} className={`${clicked ? clickedLogoStyle : 'opacity-0'} transition-all duration-500 mx-auto`}/>
       </div>
     </div>
