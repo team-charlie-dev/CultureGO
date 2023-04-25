e784503d975656b99b59d0c4c5a1229bc86b7259
Pontus <ponta.berglund.pb@gmail.com>
2023-04-25 10:29:47 +0200

    Add simple onClick function on tag cards

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TagCard({ image, text, xPosition, yPosition }) {
+  const [clicked, changeClicked] = useState('opacity-100')
+
+  function handleClick() {
+    changeClicked('opacity-50')
+  }
+
   return (
-    <div>
+    <div onClick={handleClick}>
       <div style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: `${xPosition}`, backgroundPositionY: `${yPosition}` }}
-        className='bg-primary hover:opacity-50 transition-all w-full h-full'>
+        className={`bg-primary hover:opacity-50 transition-all w-full h-full ${clicked}`}>
       </div>
       <p className='text-center bg-black bg-opacity-50 text-white w-full relative bottom-1/2 font-inriaSans italic'>{text}</p>
     </div>
