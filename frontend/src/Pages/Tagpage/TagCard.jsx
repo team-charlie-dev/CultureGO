644d95d4a9939af394ac5c02635267f7ac6510dd
Phoenix Connelin <126803905+PhoenixConnelin@users.noreply.github.com>
2023-04-25 11:28:22 +0200

    Fixed scaling

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -7,12 +7,12 @@ export default function TagCard({ image, text, xPosition, yPosition }) {
     changeClicked(clicked => !clicked)
   }
 
-  const clickedStyle = 'opacity-50 scale-125' 
+  const clickedStyle = 'opacity-50 scale-[1.4]' 
 
   return (
-    <div onClick={handleClick}>
+    <div className = 'overflow-hidden' onClick={handleClick}>
       <div style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: `${xPosition}`, backgroundPositionY: `${yPosition}` }}
-        className={`hover:opacity-50 transition-all w-full h-full ${clicked ? clickedStyle : ''}`}>
+        className={`hover:opacity-50 transition-all duration-500 w-full h-full ${clicked ? clickedStyle : ''}`}>
       </div>
       <p className='text-center bg-black bg-opacity-50 text-white w-full relative bottom-1/2 font-inriaSans italic'>{text}</p>
     </div>
