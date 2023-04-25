b0da324394fb84f7f35b18dda609561d29b03786
Pontus <ponta.berglund.pb@gmail.com>
2023-04-25 11:10:54 +0200

    Add scale to onClick

--- frontend/src/Pages/Tagpage/TagCard.jsx
+++ frontend/src/Pages/Tagpage/TagCard.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 
 export default function TagCard({ image, text, xPosition, yPosition }) {
-  const [clicked, changeClicked] = useState('opacity-100')
+  const [clicked, changeClicked] = useState(false)
 
   function handleClick() {
-    changeClicked('opacity-50')
+    changeClicked(clicked => !clicked)
   }
+
+  const clickedStyle = 'opacity-50 scale-125' 
 
   return (
     <div onClick={handleClick}>
       <div style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: `${xPosition}`, backgroundPositionY: `${yPosition}` }}
-        className={`bg-primary hover:opacity-50 transition-all w-full h-full ${clicked}`}>
+        className={`hover:opacity-50 transition-all w-full h-full ${clicked ? clickedStyle : ''}`}>
       </div>
       <p className='text-center bg-black bg-opacity-50 text-white w-full relative bottom-1/2 font-inriaSans italic'>{text}</p>
     </div>
