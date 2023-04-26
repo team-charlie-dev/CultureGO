1a44838ea883c6602454be3544b3d97045dee0f4
Rezaavoor <Reza.avoor@gmail.com>
2023-04-26 10:35:53 +0200

    fix sizes on home page

--- frontend/src/Pages/Homepage/home.jsx
+++ frontend/src/Pages/Homepage/home.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react";
 import Like from "../../Components/icons/Like.svg";
 import Dislike from "../../Components/icons/Dislike.svg";
 import Logo from "../../Components/icons/Logo.svg";
-import SelectCity from "../../Components/buttons/selectCity";
+import CityButton from "../../Components/buttons/button";
 import City from "../../Components/icons/City";
 import Arrow from "../../Components/icons/Arrow";
 
@@ -22,23 +22,23 @@ const Home = () => {
       const response = await fetch("http://localhost:4000/getitem?amount=3");
       const data = await response.json();
       setItemData({
-        name: data[1].name,
-        shortInfo: data[1].short_info,
-        images: data[1].images,
+        name: data[2].name,
+        shortInfo: data[2].short_info,
+        images: data[2].images,
       });
     }
     fetchData()
   }, []);
   
   return (
-    <div className="bg-white h-full w-full relative overflow-hidden">
+    <div className="bg-white w-full relative overflow-hidden h-[calc(100%-var(--navbar-height))]">
       <Header />
-      <div className="flex justify-end">
+      <div className="flex justify-end px-7">
         <CitySelector />
       </div>
 
-      <div className="bg-white flex flex-col h-screen">
-        <div className="relative h-[65%] w-auto font-inriaSans p-3">
+      <div className="bg-white flex flex-col h-[calc(100%-10%-1.5rem)]">
+        <div className="relative h-[85%] w-auto font-inriaSans px-3">
           <div style={{backgroundImage: 'linear-gradient(to top, rgba(255,255,255,0), rgba(255,255,255,1))', position: 'absolute', width: '100%', height: '10%'}}></div>
           <Image imgUrl={itemData.images[0]} />
           <div className="absolute bottom-0 left-0 right-0 p-3">
@@ -77,16 +77,18 @@ const InfoBox = ({name, info}) => {
 
 const Buttons = () => {
   return (
-    <div className="flex flex-row gap-[40%] justify-center">
-      <img src={Dislike}></img>
-      <img src={Like}></img>
+    <div className="h-[15%] p-4">
+      <div className="flex flex-row gap-[40%] justify-center h-full w-full">
+        <img src={Dislike}></img>
+        <img src={Like}></img>
+      </div>
     </div>
   );
 };
 
 const Header = () => {
   return (
-    <header className="p-3 pt-5">
+    <header className="p-3 pt-5 h-[10%]">
       <img src={Logo}></img>
     </header>
   );
@@ -98,7 +100,7 @@ const CitySelector = () => {
   }
 
   return (
-    <SelectCity
+    <CityButton
       text="Stockholm"
       icon={City}
       size="small"
