5e873b3ba4aae9e3078208b06219de4e347a52cf
Pontus <ponta.berglund.pb@gmail.com>
2023-04-27 09:16:42 +0200

    Add clock and wallet icon

---
+++ frontend/src/Components/icons/Clockicon.jsx
@@ -0,0 +1,17 @@
+
+export default function ClockIcon () {
+    return (
+        <svg 
+        width="24" 
+        height="25" 
+        viewBox="0 0 24 25" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M20.5 12.48C20.5 17.3919 16.6761 21.34 12 21.34C7.32388 21.34 3.5 17.3919 3.5 12.48C3.5 7.56809 7.32388 3.62 12 3.62C16.6761 3.62 20.5 7.56809 20.5 12.48Z" stroke="white"/>
+            <path d="M5 2.91599C4.08789 3.46366 3.33046 4.25139 2.80385 5.19999" stroke="white" strokeLinecap="round"/>
+            <path d="M19 2.91599C19.9121 3.46366 20.6695 4.25139 21.1962 5.19999" stroke="white" strokeLinecap="round"/>
+            <path d="M12 6.76001V12.23C12 12.3681 12.1119 12.48 12.25 12.48H16.5" stroke="white" strokeLinecap="round"/>
+        </svg>
+    )
+}
